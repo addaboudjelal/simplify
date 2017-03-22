1a2e4b655c2c60615d3d145ed174f85ebd63f8e4
Adda Boudjelal <Sofiane.Boudjelal@yrbrands.com>
2017-03-22 10:44:02 -0400

    Clear Project

--- src/app/emails.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export class Emails extends Component {
-  render() {
-    return (
-      <h1>{'Hello Emails!'}</h1>
-    );
-  }
-}
--- src/app/jobs.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export class Jobs extends Component {
-  render() {
-    return (
-      <h1>{'Hello Jobs!'}</h1>
-    );
-  }
-}
